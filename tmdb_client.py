155add30b3e2f0e34f0f95d27d4b8487bd8eebb3
Mikolaj Strzelbicki <mikolaj.strzelbicki@gmail.com>
2022-05-03 10:26:27 +0200

    Created def header().

--- tmdb_client.py
+++ tmdb_client.py
@@ -1,14 +1,16 @@
 import requests
 import random
 
-API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOWMyYzY5NzU3MmQyOThkMzFmYTQ5NzRmY2JkODNiNiIsInN1YiI6IjYyNWZiOTJhYTUwNDZlMTFlMjBkZmVhZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.r4UM54RFGq7NxDt7SXuO_oVtgwVrCsvH0POBgVryJto"
-
-def get_popular_movies():
-    endpoint = "https://api.themoviedb.org/3/movie/popular"
+def header():
+    API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOWMyYzY5NzU3MmQyOThkMzFmYTQ5NzRmY2JkODNiNiIsInN1YiI6IjYyNWZiOTJhYTUwNDZlMTFlMjBkZmVhZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.r4UM54RFGq7NxDt7SXuO_oVtgwVrCsvH0POBgVryJto"
     headers = {
         "Authorization": f"Bearer {API_TOKEN}"
     }
-    response = requests.get(endpoint, headers=headers)
+    return headers
+
+def get_popular_movies():
+    endpoint = "https://api.themoviedb.org/3/movie/popular"
+    response = requests.get(endpoint, headers=header())
     return response.json()
 
 def get_poster_url(poster_api_path, size="w342"):
@@ -22,26 +24,17 @@ def get_movies(how_many, list_type):
 
 def get_single_movie(movie_id):
     endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }
-    response = requests.get(endpoint, headers=headers)
+    response = requests.get(endpoint, headers=header())
     return response.json()
 
 def get_single_movie_cast(movie_id, how_many):
     endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }
-    response = requests.get(endpoint, headers=headers)
+    response = requests.get(endpoint, headers=header())
     return response.json()["cast"][:how_many]
 
 def get_movies_list(list_type):
     endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }
-    response = requests.get(endpoint, headers=headers)
+    response = requests.get(endpoint, headers=header())
     response.raise_for_status()
     return response.json()
 
